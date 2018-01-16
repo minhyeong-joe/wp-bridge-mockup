494fbb29601c33f7c0bf2f9cb2722fe27ca48423
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2018-01-15 20:08:17 -0800

    Home Page complete

--- scripts/script.js
+++ scripts/script.js
@@ -22,4 +22,17 @@ $(function(){
   })
 
 
+  // reservation date input default to today
+  Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+  });
+  $('.date-picker').val(new Date().toDateInputValue());
+
+  $(".date-picker").datepicker();
+  $(".date-picker").on("change", function() {
+    $(".date-picker").datepicker("option", "dateFormat", "yy-mm-dd");
+  });
+
 });
